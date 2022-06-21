f4818a5e8960947f270dba38494696a3b4f373e8
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-21 17:17:00 -0400

    import fonts

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="mint-green">
+      <h1>VAULT</h1>
+    </div>
+  );
+}
+
+export default Home;
