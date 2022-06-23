9abdc4f3de06c7987bf7e7d1c48b53ad051147c8
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-23 16:22:41 -0400

    themes menu triangles added

--- src/Menus/ThemesMenu.js
+++ src/Menus/ThemesMenu.js
@@ -10,28 +10,33 @@ const ThemesMenu = (props) => {
     }
 
     return (
-        <div style={contentStyles} className="themes-menu z-index h-one-hundred flex just-center mint-green">
-            <div className="flex al-center vault-border vault-padding">
-                <img className="ninety-vh" src="/Images/VAULT.svg" />
+        <>        
+            <div style={contentStyles} className="themes-menu z-index h-one-hundred flex just-center mint-green">
+                <div className="flex al-center mint-green vault-border vault-padding">
+                    <img className="ninety-vh" src="/Images/VAULT.svg" />
+                </div>
+                <div className="relative flex column space-btwn mint-green vault-border">
+                    <div className="flex column">
+                        <div className="menu-bttn flex jc-center pumpkin sand-txt">
+                            <h3>archival thinking</h3>
+                        </div>
+                        <div className="menu-bttn flex jc-center evergreen sand-txt">
+                            <h3>context</h3>
+                        </div>
+                        <div className="menu-bttn flex jc-center preserved-lemon sand-txt">
+                            <h3>methods</h3>
+                        </div>
+                        <div className="menu-bttn flex jc-center cobalt sand-txt">
+                            <h3>why it matters</h3>
+                        </div>
+                    </div>  
+                    <img src="/Images/MenuTriangle.svg" className="triangle" id="triangle-theme-close"/>
+                    <h3 className="vault-padding">MENU&#x2f;&#x2f;&#x2f;&#x2f;</h3>
+                </div>
+                <img src="/Images/MenuTriangle.svg" className="triangle" id="triangle-theme-open"/>
             </div>
-            <div className="flex column space-btwn mint-green vault-border">
-                <div className="flex column">
-                    <div className="menu-bttn flex jc-center pumpkin sand-txt">
-                        <h3>archival thinking</h3>
-                    </div>
-                    <div className="menu-bttn flex jc-center evergreen sand-txt">
-                        <h3>context</h3>
-                    </div>
-                    <div className="menu-bttn flex jc-center preserved-lemon sand-txt">
-                        <h3>methods</h3>
-                    </div>
-                    <div className="menu-bttn flex jc-center cobalt sand-txt">
-                        <h3>why it matters</h3>
-                    </div>
-                </div>  
-                <h3 className="vault-padding">MENU&#x2f;&#x2f;&#x2f;&#x2f;</h3>
-            </div>
-        </div>
+        </>
+
     )
 }
 
