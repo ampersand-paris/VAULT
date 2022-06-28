aa32a1e2af52a1276bbf68e3cdec9653fe945d32
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-28 17:55:11 -0400

    archival thinking through panel 3

--- src/Gathered Materials/ArchivalThinking.js
+++ src/Gathered Materials/ArchivalThinking.js
@@ -4,18 +4,60 @@ const ArchivalThinking = (props) => {
     <>
         <div className="container one-hundred-vh pumpkin">
             <div className="container-length one-hundred-vh flex">
-            <div className="intro-panel-width flex flex-end">
-                <div className="intro-width flex space-btwn column vault-padding">
-                    <div>
-                        <p className="sand-txt">&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;</p>
+                <div className="intro-panel-width flex flex-end">
+                    <div className="intro-width flex space-btwn column vault-padding">
+                        <div>
+                            <p className="sand-txt">&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;</p>
+                        </div>
+                        <p className="transcription-details sand-txt corporate"><b>Here you’ll find materials about choice-making—what to keep, who keeps it and how to keep it—along with information about how we might map an artist’s creative practice. You’ll find references to aspects of creative practice that are available outside, or even alongside the product—the dance—itself.</b></p>
                     </div>
-                    <p className="transcription-details sand-txt corporate"><b>Here you’ll find materials about choice-making—what to keep, who keeps it and how to keep it—along with information about how we might map an artist’s creative practice. You’ll find references to aspects of creative practice that are available outside, or even alongside the product—the dance—itself.</b></p>
                 </div>
-            </div>
-            <div className="ar-panel-1">
-                <h1 className="pumpkin-txt intro-title">ARCHIVAL THINKING</h1>
-                <img src="/Images/OrangeSlash.svg" className="ar-slash"/>
-            </div>
+                <div className="ar-panel-1 panel-1400 flex">
+                    <h1 className="pumpkin-txt intro-title">ARCHIVAL THINKING</h1>
+                    <img src="/Images/OrangeSlash.svg" className="ar-slash"/>
+                    <div>
+                        <div className="flex column al-center jc-center">
+                            <h2 className="pumpkin-txt"><em>How are we viewing the archive?</em></h2>
+                            <div className="flex">
+                                <div className="button-div-height flex jc-center al-center sand-border-right">
+                                    <a className="small-bttn pumpkin sand-txt">Transcription</a>
+                                </div>
+                                <div className="button-div-height flex jc-center al-center">
+                                    <a className="small-bttn pumpkin sand-txt">Video</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sand stnd-padding">
+                            <h3 className="pumpkin-txt">ESSAY: Ishmael Houston-Jones</h3> 
+                            <p className="pumpkin-txt">The process of my practice seems so fragile and elusive to the idea of archive</p>
+                        </div>
+                    </div>     
+                </div>
+                <div className="panel-700 flex column">
+                    <div className="h-fifty stnd-padding sand pumpkin-txt">
+                        <p><b>Dayna Hanson, Seattle:</b></p>
+                        <p>[We] think about media as being inherently archival, and inherently lasting; and we work in live forms that are inherently ephemeral. So [...] there's something rich and unexpected to me about these digital forms like Snapchat or like Instagram Live that expire so quickly [...] That's somehow good, maybe. Maybe it's placing a [value] judgment.</p>
+                        <a className="small-bttn pumpkin sand-txt">Video</a>
+                    </div>
+                    <div className="h-fifty stnd-padding sand-txt">
+                        <p><b>Bebe Miller, NYC:</b></p> 
+                        <p className="sand-border-right">There’s now a very different horizontal platform in the idea of archiving... The immediacy of the archive now—broadcast it, be done with it, let it be absorbed into the artistic mindset of the culture—even at the beginning of conceiving of the idea/event/artwork. In that difference between the vertical and horizontal, on a fascial/body level, is there a difference in the work we make, do we need to support both axes or yet a new direction?</p>
+                        <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
+                    </div>
+                </div>
+                <div className="panel-700 flex column">
+                    <div className="h-fifty stnd-padding sand-txt">
+                        <div className="h-one-hundred sand-border-bottom">
+                            <p><b>Dayna Hanson, Seattle:</b></p>
+                            <p>[We] think about media as being inherently archival, and inherently lasting; and we work in live forms that are inherently ephemeral. So [...] there's something rich and unexpected to me about these digital forms like Snapchat or like Instagram Live that expire so quickly [...] That's somehow good, maybe. Maybe it's placing a [value] judgment.</p>
+                            <a className="small-bttn pumpkin-txt sand">Transcription</a>
+                        </div>
+                    </div>
+                    <div className="h-fifty stnd-padding sand-txt">
+                        <p><b>Ann Carlson, NYC:</b></p> <p>The search for the right word [...] is interesting. “Archiving is really not quite right….” What I hear in the search for the word, and also in the fascia, is value; trapping, mapping understanding value. You’re also immediately saying ‘the lifetime of this effort that I’m putting out in the world, what is its resonance?” I love the looking for the word that is the thing, the embodied connection…what is fascia? It changes, responds differently between body and bone, it’s self-supporting… Something about the word resonant, resonant meaning perhaps – what we are, what we do… I love the reach for the word. The horizontal and vertical thing, it’s the present-moment awareness that the song will keep going.</p>
+                        <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
+                    </div>
+                </div>
             </div>
         </div>
     </>
