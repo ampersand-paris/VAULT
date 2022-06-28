a720de48e94f6bf75d4ff7bd5a46c43cc0717b17
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-28 09:15:11 -0400

    archival thinking setup

---
+++ src/Gathered Materials/ArchivalThinking.js
@@ -0,0 +1,25 @@
+const ArchivalThinking = (props) => {
+
+    return (
+    <>
+        <div className="container one-hundred-vh pumpkin">
+            <div className="container-length one-hundred-vh flex">
+            <div className="intro-panel-width flex flex-end">
+                <div className="intro-width flex space-btwn column vault-padding">
+                    <div>
+                        <p className="sand-txt">&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;</p>
+                    </div>
+                    <p className="transcription-details sand-txt corporate"><b>Here you’ll find materials about choice-making—what to keep, who keeps it and how to keep it—along with information about how we might map an artist’s creative practice. You’ll find references to aspects of creative practice that are available outside, or even alongside the product—the dance—itself.</b></p>
+                </div>
+            </div>
+            <div className="ar-panel-1">
+                <h1 className="pumpkin-txt intro-title">ARCHIVAL THINKING</h1>
+                <img src="/Images/OrangeSlash.svg" className="ar-slash"/>
+            </div>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default ArchivalThinking;
