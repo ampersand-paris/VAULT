f0dfd82a612ca163fa20eb1728c2f360579eec18
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-30 11:14:10 -0400

    archival thinking desktop v1 final

--- src/Gathered Materials/ArchivalThinking.js
+++ src/Gathered Materials/ArchivalThinking.js
@@ -19,10 +19,10 @@ const ArchivalThinking = (props) => {
                         <div className="flex column al-center jc-center top-100px">
                             <h2 className="pumpkin-txt"><em>How are we viewing the archive?</em></h2>
                             <div className="flex">
-                                <div className="button-div-height flex jc-center al-center sand-border-right">
+                                <div className="button-div-height flex jc-center al-center vault-padding sand-border-right">
                                     <a className="small-bttn pumpkin sand-txt">Transcription</a>
                                 </div>
-                                <div className="button-div-height flex jc-center al-center">
+                                <div className="button-div-height flex jc-center al-center vault-padding">
                                     <a className="small-bttn pumpkin sand-txt">Video</a>
                                 </div>
                             </div>
@@ -49,18 +49,132 @@ const ArchivalThinking = (props) => {
                         <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
                     </div>
                 </div>
-                <div className="panel-700 flex column">
-                    <div className="h-fifty stnd-padding sand-txt">
-                        <div className="h-one-hundred sand-border-bottom">
-                            <p><b>Dayna Hanson, Seattle:</b></p>
-                            <p>[We] think about media as being inherently archival, and inherently lasting; and we work in live forms that are inherently ephemeral. So [...] there's something rich and unexpected to me about these digital forms like Snapchat or like Instagram Live that expire so quickly [...] That's somehow good, maybe. Maybe it's placing a [value] judgment.</p>
-                            <a className="small-bttn pumpkin-txt sand">Transcription</a>
+                <div className="panel-900 flex column">
+                    <div className="h-fifty vault-padding ">
+                        <div className="stnd-padding sand-txt sand-border-bottom">
+                            <p><b>Ann Carlson, NYC:</b></p> <p>The search for the right word [...] is interesting. “Archiving is really not quite right….” What I hear in the search for the word, and also in the fascia, is value; trapping, mapping understanding value. You’re also immediately saying ‘the lifetime of this effort that I’m putting out in the world, what is its resonance?” I love the looking for the word that is the thing, the embodied connection…what is fascia? It changes, responds differently between body and bone, it’s self-supporting… Something about the word resonant, resonant meaning perhaps – what we are, what we do… I love the reach for the word. The horizontal and vertical thing, it’s the present-moment awareness that the song will keep going.</p>
+                            <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
                         </div>
                     </div>
-                    <div className="h-fifty stnd-padding sand-txt">
-                        <p><b>Ann Carlson, NYC:</b></p> <p>The search for the right word [...] is interesting. “Archiving is really not quite right….” What I hear in the search for the word, and also in the fascia, is value; trapping, mapping understanding value. You’re also immediately saying ‘the lifetime of this effort that I’m putting out in the world, what is its resonance?” I love the looking for the word that is the thing, the embodied connection…what is fascia? It changes, responds differently between body and bone, it’s self-supporting… Something about the word resonant, resonant meaning perhaps – what we are, what we do… I love the reach for the word. The horizontal and vertical thing, it’s the present-moment awareness that the song will keep going.</p>
-                        <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
+                    <div className="h-fifty flex space-btwn stnd-padding sand-txt">
+                        <div className="w-forty-seven">
+                            <img className="" src="/Images/Pictures/Reggie.svg" />
+                            <div className="stnd-padding border-radius sand pumpkin-txt">
+                                <p className="transcription-details no-padding"><b>Reggie Wilson, Seattle:</b></p>
+                                <p className="transcription-details no-padding">Is this for our own dramaturgy? Is this for another choreographer’s dramaturgy? Is this for historians?</p>
+                                <div className="flex flex-end">
+                                    <a className="small-bttn pumpkin sand-txt">Video</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-forty-seven">
+                            <img className="" src="/Images/Pictures/DanaWhitco.svg" />
+                            <div className="stnd-padding border-radius sand pumpkin-txt">
+                                <p className="transcription-details no-padding"><b>Dana Whitco:</b></p>
+                                <p className="transcription-details no-padding">That choreography could be thought of not as the creation of steps that have its own sort of logic, but that it is a wayfinding technique In the world.</p>
+                                <div className="flex flex-end">
+                                    <a className="small-bttn pumpkin sand-txt">Video</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div className="panel-1400 flex column">
+                    <div className="h-fifty flex">
+                        <div className="w-seventy sand-border-right">
+                            <div className="flex space-btwn">
+                                <div className="flex w-seventy">
+                                    <div className="w-fifty stnd-padding">
+                                        <img src="/Images/Pictures/LaRocco.svg" />
+                                    </div>
+                                    <div className="relative stnd-padding">
+                                        <div className="flex column jc-center al-center double-padding sand pumpkin-txt">
+                                            <p className="transcription-details side-padding no-padding"><b>WRITING: Claudia La Rocco</b></p>
+                                            <p className="transcription-details side-padding no-padding txt-al-center">There is often anxiety around documentation.</p>
+                                        </div>
+                                        <div className="flex jc-center sand-gradient-50-down w-one-hundred">
+                                            <a className="small-bttn pumpkin sand-txt sand-border">Writing</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex column space-btwn stnd-padding">
+                                    <div className="sand pumpkin-txt stnd-padding border-radius">
+                                        <p className="transcription-details no-padding">Is there a way to enlist [funders] in learning from this process…</p>
+                                        <div className="flex flex-end">
+                                            <a className="small-bttn pumpkin sand-txt">Video</a>
+                                        </div>
+                                    </div>
+                                    <img className="only-top-padding" src="/Images/Pictures/Reggie.svg" />
+                                </div>                    
+                            </div>
+                        </div>
+                        <div className="w-twenty-five flex column space-btwn al-center stnd-padding">
+                            <img className="only-top-padding" src="/Images/Pictures/Reggie.svg" />
+                            <div className="sand pumpkin-txt border-radius stnd-padding ">
+                                <p className="no-padding transcription-details"><b>Jaamil Olawale Kosoko</b></p> 
+                                <p className="no-padding transcription-details">...as the archivist, as the curator, as the maker, I'm just showing you my ways in which I'm maneuvering through certain materials.</p>
+                                <div className="flex flex-end">
+                                    <a className="small-bttn pumpkin sand-txt">Video</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-fifty flex">
+                        <div className="w-sixty">
+                            <div className="flex h-one-hundred">
+                                <div className="bebe-seattle w-forty-seven stnd-padding">
+                                    <div className="flex flex-end">
+                                        <a className="small-bttn pumpkin sand-txt">Meeting Notes</a>
+                                    </div>
+                                </div>
+                                <div className="w-forty-seven stnd-padding sand-txt sand-border">
+                                    <p><b>Bebe Miller, Seattle:</b></p> 
+                                    <p>Did we grow into this archiving of process? 30 years ago we only documented performance even though all that process was there. We have learned to love it and want to share that. When we stop wanting to do that, share that ephemeral aspect of what we love about process? What else is there? Our attention to process is a kind of thing.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-forty flex column flex-end al-center double-padding w-one-hundred">
+                            <div className="flex flex-end w-one-hundred stnd-padding">
+                                <img className="w-forty" src="/Images/Pictures/Jaamil.svg" />
+                            </div>
+                            <div className="flex jc-center sand-gradient-50 w-one-hundred">
+                                <a className="small-bttn pumpkin sand-txt sand-border">Writing</a>
+                            </div>
+                            <div className="sand pumpkin-txt w-one-hundred">
+                                <p className="side-padding"><b>WRITING: Dayna Hanson</b></p>
+                                <p className="side-padding">…documentation has become more of an aesthetic pursuit…</p>
+                            </div>
+                            <h3 className="vault-padding sand-txt">&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="panel-700 flex column">
+                    <div className="h-fifty flex space-btwn stnd-padding">
+                        <div className="w-forty-seven">
+                                <div className="sand-txt">
+                                    <p className="no-padding"><b>Raja Feather Kelly, Seattle:</b></p>
+                                    <p className="no-padding">Would other/newer ways to archive, change the way work was made?</p>
+                                    <div className="flex">
+                                        <a className="small-bttn sand pumpkin-txt">Meeting notes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-forty-seven">
+                                <img className="" src="/Images/Pictures/Reggie.svg" />
+                                <div className="stnd-padding border-radius sand pumpkin-txt">
+                                    <p className="transcription-details no-padding"><b>Bebe Miller:</b></p>
+                                    <p className="transcription-details no-padding">Bebe Miller, …there's something about the immediacy of [social media's] dispersal of information...</p>
+                                    <div className="flex flex-end">
+                                        <a className="small-bttn pumpkin sand-txt">Video</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <div className="h-fifty sand pumpkin-txt stnd-padding">
+                        <p><b>Judy Hussie-Taylor, NYC: </b></p>
+                        <p>Intersection between what are we doing, what are we saving, what are we collecting, what are we not doing—always at play in our work at least on the curatorial side. We’re making interventions, we’re making choices. What we all feel every day: whatever it is we value is under attack, in so many ways. A war on certain kinds of value systems. Maybe this is different than how/what we archive but these choices point to what we value.</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
