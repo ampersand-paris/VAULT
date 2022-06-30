99c6849df1824c0a3b01d3e5292817dccaec6ee1
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-30 12:15:51 -0400

    bio mobile

--- src/Biographies/MarjiCole.js
+++ src/Biographies/MarjiCole.js
@@ -4,8 +4,8 @@ const MarjiColeBio = (props) => {
 
     return (
     <>
-        <div className="one-hundred-vh flex  jc-center top-padding preserved-lemon">
-            <div className="vault-padding">
+        <div className="one-hundred-vh flex  jc-center mobile-al-center mobile-column top-padding preserved-lemon ">
+            <div className="vault-padding mobile-flex mobile-jc-center mobile-width">
                 <div className="flex space-btwn essay-title-height">
                     <div>
                         <h2 className="subtitle ">Essay</h2>
@@ -16,7 +16,7 @@ const MarjiColeBio = (props) => {
                     </div>
                 </div>
             </div>
-            <section className="flex column al-center no-top-padding preserved-lemon">
+            <section className="flex column al-center no-top-padding mobile-padding preserved-lemon">
                 <div className="essay-width ">
                     <p className="no-top-padding"><a href="http://www.margicole.com/">Margi Cole</a> founder and Artistic Director of The Dance COLEctive, received a BA from Columbia College Chicago and an MFA from the University of Illinois at Urbana-Champaign. She has taught and performed nationally and internationally. Active in the Chicago dance community for over 30 years, she has served as a Chicago Dancemakers Forum Consortium Member, on grant panels, in public forums and on the board of See Chicago Dance. Cole was on faculty at Columbia College Chicago, where she has served as a Lecturer and Associate Chair and as Program Manager of the Dance Center Presenting Series. She currently teaches at the Alabama School of Fine Arts. funders acknowledge your worth by the number of performances you do, size of the venue and the audience. The importance, worth and value of 1 person (especially a mid-career artist) receiving support to do something and the perception that it will only impact that single person…</p>
                 </div>
