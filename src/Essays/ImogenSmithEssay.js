4f3e546412e14c6b776c0afbcae0f5b52fb0f8cb
ampersand-paris <ampersand.s.paris@gmail.com>
2022-07-21 13:19:45 -0400

    essays done

---
+++ src/Essays/ImogenSmithEssay.js
@@ -0,0 +1,49 @@
+// ====== COMPONENTS ======
+
+const ImogenSmithEssay = (props) => {
+
+    return (
+    <>
+        <div className="top-padding flex jc-center mobile-column cobalt sand-txt ">
+            <div className="vault-padding mobile-jc-center mobile-flex">
+                <div className="relative essay-title-height mobile-width  flex  space-btwn ">
+                    <div>
+                        <h2 className="subtitle essay-heading">Essay</h2>
+                    </div>
+                    <img className="essay-slash" src="/Images/Slash.svg" />
+                    <div className="flex column flex-end">
+                        <h2 className="subtitle essay-author">Imogen Smith</h2>
+                    </div>
+                </div>
+            </div>
+            <section className="flex column al-center mobile-padding">
+                <div className="essay-width">
+                    <p className="no-top-padding"><b>How has your dance-making been affected (or not) by the virtual/digital availability of your work, in terms of both process and product?</b></p>
+                    <p>My observation as an archivist has been that artists often lack access to documentation they need to remount works, largely due to the prevalence of obsolete analog video formats. They expend excessive time trying to find materials they need for re-staging, marketing, and engagement. Particularly with the decline in dance criticism in newspapers, artists feel more responsible for educating audiences about their work, which requires mining legacy materials. I have also observed that artists can gain valuable new perspective on their works and the arc of their career through the archiving process. Lastly, educators urgently need more access to full-length recordings of dance works for teaching, and the dearth of such recordings affects the next generation of dance-makers and their dance literacy.</p>
+                    <p><b>What strategies and experiences can you share related to digital or analog archiving and your current practice?</b></p>
+                    <p>During my tenure with Dance Heritage Coalition and now with Dance/USA, I have participated in and observed a variety of successful strategies and resources for archiving. These include:</p>
+                    <p>The Artist’s Legacy Toolkit, which consists of free online guides, tips, tools, and templates aimed at providing a reliable source of information and adaptable tools to help artists organize, preserve, and manage their analog and digital records.</p>
+                    <p>Hands-on assistance to artists and companies, funded by grants or through contracts, including:</p>
+                    <ul>
+                        <li className="bullets">Archive assessments, in which an archival consultant completes and site visit and interviews artists and/or company staff, and creates a report giving a high-level description of the scope and condition of the archive along with prioritized recommendations for actions to improve security and accessibility of archives.</li>
+                        <li className="bullets">Inventories: more detailed descriptions of collections, at the item level for AV materials and box/drawer level for paper materials. Inventories improve intellectual control of archives and enable prioritization of materials for digitization.</li>
+                        <li className="bullets">Digitization projects: migration of obsolete video formats (including analog and digital magnetic media and optical media) to archival-quality digital file formats; scanning of paper and photo materials, including slides and oversize posters; development of long-term storage plans for digital assets.</li>
+                        <li className="bullets">Creation of databases and digital asset management systems to allow organizations to efficiently find and access digital and analog materials.</li>
+                    </ul>
+                    <p><b>What do you assume about audience in this regard?</b></p>
+                    <p>If I’m interpreting this question correctly: I would assume that the audience for dance archives includes artists, educators, students, and the general public, and that accessibility of archives affects the visibility and appreciation of dance, and as such affects the health of the field.</p>
+                    <p><b>Considering the spectrum from independent artist to supporting institutions, what are the directional shifts you’d like to see? Examples: funding for archiving; educational/training support; advocacy; networking between artists; a collective clearinghouse/library; public access; etc.</b></p>
+                    <p>All of the above! My top priorities are:</p>
+                    <p>Support for digitizing at-risk AV materials and improving security of digital assets through partnerships with trusted institutions;</p>
+                    <p>Community-based workshops to strengthen archiving and records management skills and build peer-to-peer support networks;</p>
+                    <p>Hands-on help for artists and dance orgs from archival Fellows or consultants to provide assessments, set priorities, create feasible work-plans, and train staff</p>
+                    <p>I would also like to see a commitment from artists to the value of broad access to their work, and an agreement on shared standards for describing dance collections;</p>
+                    <p>Funding for all of the above!</p>
+                </div>
+            </section>
+        </div>
+    </>
+  );
+}
+
+export default ImogenSmithEssay;
---
+++ src/Essays/RussellLepleyEssay.js
@@ -0,0 +1,43 @@
+// ====== COMPONENTS ======
+
+const RussellLepleyEssay = (props) => {
+
+    return (
+    <>
+        <div className="top-padding flex jc-center mobile-column preserved-lemon ">
+            <div className="vault-padding mobile-jc-center mobile-flex">
+                <div className="relative essay-title-height mobile-width  flex  space-btwn ">
+                    <div>
+                        <h2 className="subtitle essay-heading">Essay</h2>
+                    </div>
+                    <img className="essay-slash" src="/Images/Slash.svg" />
+                    <div className="flex column flex-end">
+                        <h2 className="subtitle essay-author">Russell Lepley</h2>
+                    </div>
+                </div>
+            </div>
+            <section className="flex column al-center mobile-padding">
+                <div className="essay-width">
+                    <p className="no-top-padding"><b>1.</b></p>
+                    <p>Product:</p>
+                    <p>I am an early career dance-maker and have documentation of my complete works publicly available on streaming platforms Youtube and Vimeo. This documentation has been useful when applying for fellowships, awards and grants. Attention to how my work is documented affects how institutions perceive my product which influences my future work directly. Funding from a presenter or a grant/award allows me to dedicate my time to the process rather than strategizing a marketing and fundraising campaign. My work becomes more thoughtfully layered and more concise without those pressures. Further, documentation affects whether a work will be given opportunities to evolve through touring and develop in residencies.</p>
+                    <p>Fragments of a process or performance on social media seem to be a new and evolving form of archiving. It connects choreographers not only with a local audience but an online dance community and has an impact on job opportunities and networking. Within the concert dance world, peers have been hired for full-length commissions on social media content alone and FluxFlow Dance Project has been contacted by performers interested in working with the company based on our media output.</p>
+                    <p>Process:</p>
+                    <p>Documenting process digitally has become habitual for me. It enables me to move an idea from a notecard to film. I structure my work as a string of scenes which in process are rarely created chronologically. The ability to shift my attention to a new idea without fear of losing work enables me to compartmentalize then organize later. This has helped me develop thematically related material that varies significantly in form or genre but which can be assembled later into a cohesive whole.</p>
+                    <p><b>2.</b></p>
+                    <p>The vast majority of my movement generation comes from transposing melodies or texts to corresponding movement. I rely on video documentation to ensure details are not lost. I did this initially to work towards whatever performance was at hand but I have since repurposed archived material in different contexts—a piece I made for six professional dancers in Germany 2016 was reimagined for 19 adult beginners in Columbus in 2019. I’m curious what possibilities that sort of movement time travel will offer me as a maker 10, 20, 30 years from now.</p>
+                    <p><b>3.</b></p>
+                    <p>As a maker, I’m focused on creating work with live performers engaging with a live audience.</p>
+                    <p>As an archiver of that work I have of three audiences.</p>
+                    <p>First, I am aiming to capture a work’s energy and essence for institution leaders and dance company directors in pursuit of job opportunities. I’m assuming they have seen a lot of dance film from well-funded organizations and my video needs to be a high quality to demonstrate professionalism and preparedness for their stages.</p>
+                    <p>Another audience is our social media following. I’m assuming they need a more striking visual and have a smaller attention span given the nature of those platforms. Finally, I think of myself and my collaborators as material for analysis, organization, composition. We need clear and simply shot video and a system in place to conveniently access videos.</p>
+                    <p><b>4.</b></p>
+                    <p>Increased funding and particularly networking assistance would be useful for newer artists. FluxFlow Dance Project’s boon is that its space has a community full of artists. From that network, we found our videographers early on and that collaboration has been going strong for two years. When working on a limited budget and having many other life/work elements to juggle, a relationship of trust is invaluable. Finding more ways to build those relationships seems important.</p>
+                </div>
+            </section>
+        </div>
+    </>
+  );
+}
+
+export default RussellLepleyEssay;
