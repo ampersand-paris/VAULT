66bc28513bb2efaaa7ddc1f9816da78557eb648e
ampersand-paris <ampersand.s.paris@gmail.com>
2022-07-27 11:51:37 -0400

    mobile front-end

--- src/Gathered Materials/ArchivalThinking.js
+++ src/Gathered Materials/ArchivalThinking.js
@@ -3,8 +3,8 @@ const ArchivalThinking = (props) => {
     return (
     <>
         <div className="container one-hundred-vh pumpkin">
-            <div className="container-length one-hundred-vh flex">
-                <div className="intro-panel-width flex flex-end">
+            <div className="container-length one-hundred-vh flex mobile-column">
+                <div className="intro-panel-width flex flex-end mobile-jc-center">
                     <div className="intro-width flex space-btwn column vault-padding">
                         <div>
                             <p className="sand-txt">&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;&#x2f;</p>
@@ -12,10 +12,10 @@ const ArchivalThinking = (props) => {
                         <p className="transcription-details sand-txt corporate"><b>Here you’ll find materials about choice-making—what to keep, who keeps it and how to keep it—along with information about how we might map an artist’s creative practice. You’ll find references to aspects of creative practice that are available outside, or even alongside the product—the dance—itself.</b></p>
                     </div>
                 </div>
-                <div className="ar-panel-1 panel-1400 flex">
+                <div className="ar-panel-1 panel-1400 flex mobile-column mobile-side-padding">
                     <h1 className="pumpkin-txt intro-title">ARCHIVAL THINKING</h1>
-                    <img src="/Images/OrangeSlash.svg" className="ar-slash"/>
-                    <div className="h-one-hundred flex column space-btwn">
+                    <img src="/Images/OrangeSlash.svg" className="ar-slash display-none"/>
+                    <div className="h-one-hundred flex column">
                         <div className="flex column al-center jc-center top-100px">
                             <h2 className="pumpkin-txt"><em>How are we viewing the archive?</em></h2>
                             <div className="flex">
@@ -27,17 +27,17 @@ const ArchivalThinking = (props) => {
                                 </div>
                             </div>
                         </div>
-                        <div className="h-fifty relative  bottom-100px">
+                        <div className="h-fifty relative mobile-flex mobile-column bottom-100px mobile-top-100px">
                             <div className="z-index-10 relative w-fifty sand stnd-padding">
                                 <h3 className="pumpkin-txt">WRITING: Ishmael Houston-Jones</h3> 
                                 <p className="pumpkin-txt">The process of my practice seems so fragile and elusive to the idea of archive</p>
                                 <a className="small-bttn pumpkin sand-txt">Writing</a>
                             </div>
-                            <img src="/Images/Pictures/Ishmael.svg" className="ishmael"/>
+                            <img src="/Images/Pictures/Ishmael.svg" className="ishmael mobile-width"/>
                         </div>
                     </div>     
                 </div>
-                <div className="panel-700 flex column">
+                <div className="panel-700 flex column mobile-side-padding">
                     <div className="h-fifty stnd-padding sand pumpkin-txt">
                         <p><b>Dayna Hanson, Seattle:</b></p>
                         <p>[We] think about media as being inherently archival, and inherently lasting; and we work in live forms that are inherently ephemeral. So [...] there's something rich and unexpected to me about these digital forms like Snapchat or like Instagram Live that expire so quickly [...] That's somehow good, maybe. Maybe it's placing a [value] judgment.</p>
@@ -45,19 +45,19 @@ const ArchivalThinking = (props) => {
                     </div>
                     <div className="h-fifty stnd-padding sand-txt">
                         <p><b>Bebe Miller, NYC:</b></p> 
-                        <p className="sand-border-right">There’s now a very different horizontal platform in the idea of archiving... The immediacy of the archive now—broadcast it, be done with it, let it be absorbed into the artistic mindset of the culture—even at the beginning of conceiving of the idea/event/artwork. In that difference between the vertical and horizontal, on a fascial/body level, is there a difference in the work we make, do we need to support both axes or yet a new direction?</p>
+                        <p className="sand-border-right no-mobile-border">There’s now a very different horizontal platform in the idea of archiving... The immediacy of the archive now—broadcast it, be done with it, let it be absorbed into the artistic mindset of the culture—even at the beginning of conceiving of the idea/event/artwork. In that difference between the vertical and horizontal, on a fascial/body level, is there a difference in the work we make, do we need to support both axes or yet a new direction?</p>
                         <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
                     </div>
                 </div>
-                <div className="panel-900 flex column">
+                <div className="panel-900 flex column mobile-side-padding top-mobile-sand-border">
                     <div className="h-fifty vault-padding ">
-                        <div className="stnd-padding sand-txt sand-border-bottom">
+                        <div className="stnd-padding sand-txt sand-border-bottom no-mobile-border">
                             <p><b>Ann Carlson, NYC:</b></p> <p>The search for the right word [...] is interesting. “Archiving is really not quite right….” What I hear in the search for the word, and also in the fascia, is value; trapping, mapping understanding value. You’re also immediately saying ‘the lifetime of this effort that I’m putting out in the world, what is its resonance?” I love the looking for the word that is the thing, the embodied connection…what is fascia? It changes, responds differently between body and bone, it’s self-supporting… Something about the word resonant, resonant meaning perhaps – what we are, what we do… I love the reach for the word. The horizontal and vertical thing, it’s the present-moment awareness that the song will keep going.</p>
                             <a className="small-bttn pumpkin-txt sand">Meeting Notes</a>
                         </div>
                     </div>
                     <div className="h-fifty flex space-btwn stnd-padding sand-txt">
-                        <div className="w-forty-seven">
+                        <div className="w-forty-seven w-forty-seven-split">
                             <img className="" src="/Images/Pictures/Reggie.svg" />
                             <div className="stnd-padding border-radius sand pumpkin-txt">
                                 <p className="transcription-details no-padding"><b>Reggie Wilson, Seattle:</b></p>
@@ -67,7 +67,7 @@ const ArchivalThinking = (props) => {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-forty-seven">
+                        <div className="w-forty-seven w-forty-seven-split">
                             <img className="" src="/Images/Pictures/DanaWhitco.svg" />
                             <div className="stnd-padding border-radius sand pumpkin-txt">
                                 <p className="transcription-details no-padding"><b>Dana Whitco:</b></p>
@@ -79,18 +79,18 @@ const ArchivalThinking = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="panel-1400 flex column">
-                    <div className="h-fifty flex">
+                <div className="panel-1400 flex column mobile-side-padding">
+                    <div className="h-fifty flex mobile-column">
                         <div className="w-seventy sand-border-right">
-                            <div className="flex space-btwn">
-                                <div className="flex w-seventy">
+                            <div className="flex space-btwn mobile-column">
+                                <div className="flex mobile-column w-seventy">
                                     <div className="w-fifty stnd-padding">
                                         <img src="/Images/Pictures/LaRocco.svg" />
                                     </div>
                                     <div className="relative stnd-padding">
-                                        <div className="flex column jc-center al-center double-padding sand pumpkin-txt">
+                                        <div className="flex column jc-center double-padding sand pumpkin-txt">
                                             <p className="transcription-details side-padding no-padding"><b>WRITING: Claudia La Rocco</b></p>
-                                            <p className="transcription-details side-padding no-padding txt-al-center">There is often anxiety around documentation.</p>
+                                            <p className="transcription-details side-padding no-padding">There is often anxiety around documentation.</p>
                                         </div>
                                         <div className="flex jc-center sand-gradient-50-down w-one-hundred">
                                             <a className="small-bttn pumpkin sand-txt sand-border">Writing</a>
@@ -119,11 +119,11 @@ const ArchivalThinking = (props) => {
                             </div>
                         </div>
                     </div>
-                    <div className="h-fifty flex">
+                    <div className="h-fifty flex mobile-column">
                         <div className="w-sixty">
-                            <div className="flex h-one-hundred">
-                                <div className="bebe-seattle w-forty-seven stnd-padding">
-                                    <div className="flex flex-end">
+                            <div className="flex h-one-hundred mobile-column">
+                                <div className="bebe-seattle w-forty-seven mobile-w-forty-seven-border-adjust stnd-padding">
+                                    <div className="flex flex-end mobile-h-200px">
                                         <a className="small-bttn pumpkin sand-txt">Meeting Notes</a>
                                     </div>
                                 </div>
@@ -134,7 +134,7 @@ const ArchivalThinking = (props) => {
                             </div>
                         </div>
                         <div className="w-forty flex column flex-end al-center double-padding w-one-hundred">
-                            <div className="flex flex-end w-one-hundred stnd-padding">
+                            <div className="flex flex-end mobile-jc-center w-one-hundred stnd-padding">
                                 <img className="w-forty" src="/Images/Pictures/Jaamil.svg" />
                             </div>
                             <div className="flex jc-center sand-gradient-50 w-one-hundred">
@@ -148,9 +148,9 @@ const ArchivalThinking = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="panel-700 flex column">
+                <div className="panel-700 flex column mobile-side-padding">
                     <div className="h-fifty flex space-btwn stnd-padding">
-                        <div className="w-forty-seven">
+                        <div className="w-forty-seven w-forty-seven-split">
                                 <div className="sand-txt">
                                     <p className="no-padding"><b>Raja Feather Kelly, Seattle:</b></p>
                                     <p className="no-padding">Would other/newer ways to archive, change the way work was made?</p>
@@ -159,7 +159,7 @@ const ArchivalThinking = (props) => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-forty-seven">
+                            <div className="w-forty-seven w-forty-seven-split">
                                 <img className="" src="/Images/Pictures/Reggie.svg" />
                                 <div className="stnd-padding border-radius sand pumpkin-txt">
                                     <p className="transcription-details no-padding"><b>Bebe Miller:</b></p>
