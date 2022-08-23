045e8eb0edfcfe9867913d0a52cea8ba4108ac86
ampersand-paris <ampersand.s.paris@gmail.com>
2022-08-23 17:03:07 -0400

    why it matters panel 2

--- src/Gathered Materials/WhyItMatter.js
+++ src/Gathered Materials/WhyItMatter.js
@@ -48,7 +48,6 @@ const WhyItMatters = (props) => {
                         <img src="/Images/Pictures/WhyItMattersImg2.png" className="why-it-matters-img-2 absolute z-index-10" />
                         <div className="white-box absolute stnd-padding sand-border-left">
                             <div className="sand white-box">
-
                             </div>
                         </div>
                         <div className="h-fifty flex jc-baseline space-btwn z-index-11">
@@ -65,56 +64,65 @@ const WhyItMatters = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="panel-1800 flex mobile-column mobile-side-padding top-mobile-sand-border">
-                    <div className="w-twenty-five flex column space-btwn stnd-padding">
-                        <div className="flex column al-flex-end">
-                            <div className="w-seventy-five flex sand-gradient-50 w-one-hundred">
-                                <a className="small-bttn left-margin-10px black sand-txt sand-border">Writing</a>
-                            </div>
-                            <div className="w-seventy-five sand w-one-hundred">
-                                <p className="side-padding"><b>WRITING: J’Sun Howard</b></p>
-                                <p className="side-padding">Virtual Reality/Augmented Reality is becoming a new trend in dance…</p>
+                <div className="panel-1800 flex space-btwn mobile-column mobile-side-padding top-mobile-sand-border">
+                    <div className="w-thirty flex column space-around">
+                        <div className="relative h-200px  flex column flex-end">
+                            <a className="absolute top-30px small-bttn left-margin-10px cobalt sand-txt sand-border">Writing</a>
+                            <div className="h-150px w-ninety flex column jc-center al-center stnd-padding sand-txt sand-border">
+                                <p className="no-padding"><b>Ann Carlson</b></p>
+                                <p className="no-top-padding txt-al-center">I assume that there is a much larger audience/spectatorship for works that are mediated for viewing on a personal computer.</p>
                             </div>
                         </div>
-                        <div className="flex column al-flex-end mobile-top-20px txt-al-right">
-                            <p className="no-padding"><b>Angie Hauser</b></p> 
-                            <p className="no-padding">…whatever direction I might go in, [in order to] participate in a conversation around archiving, and to build process around archives, it would be to align [archiving] with how dance is actually made</p>
-                            <a className="small-bttn jc-center sand black-txt">VIDEO</a>
-                            <img src="/Images/Pictures/Reggie.svg" className="only-top-padding" />
+                        <img src="/Images/Pictures/Reggie.svg" className="w-eighty" />
+                        <div className="stnd-padding sand-txt">
+                            <p className="no-padding"><b>Jacob’s Pillow, Jaamil Olawale Kosoko:</b></p> 
+                            <p className="no-padding">What's the criticality behind what my offering is? With that comes a kind of cultural diplomacy, or ambassadorship.</p>
+                            <a className="small-bttn jc-center sand cobalt-txt top-margin-10px">Video</a>
                         </div>
                     </div>
-                    <div className="w-seventy-five flex column stnd-padding">
-                        <div className="h-sixty flex mobile-column space-btwn stnd-padding">
-                            <div className="w-forty flex column space-around">
-                                <div className="flex column">
-                                    <div className="flex sand-gradient-50 w-one-hundred">
-                                        <a className="small-bttn left-margin-10px black sand-txt sand-border">Writing</a>
-                                    </div>
-                                    <div className="w-one-hundred sand">
-                                        <p className="side-padding"><b>WRITING: Lauren Slone</b></p>
-                                        <p className="side-padding">…I also get into strange conversations with myself about “proving” to friends that I’m doing something.</p>
-                                    </div>
-                                </div>
-                                <div className="flex column mobile-top-20px">
-                                    <div className="flex sand-gradient-50 w-one-hundred">
-                                        <a className="small-bttn left-margin-10px black sand-txt sand-border">Writing</a>
-                                    </div>
-                                    <div className="w-one-hundred sand">
-                                        <p className="side-padding"><b>WRITING: Erin Kilmurray</b></p>
-                                        <p className="side-padding">…peer/follower review + feedback + championing has been the active ingredient in pushing [my work] forward.</p>
-                                    </div>
-                                </div>
+                    <div className="relative w-thirty flex column flex-end stnd-padding sand-txt">
+                        <div className="relative h-fifty">
+                            <img src="/Images/Pictures/WhyItMattersImg3.png" className="h-one-hundred"/>
+                            <p className="absolute bottom-10px left-10px no-padding"><b>Columbus: Gesel Mason:</b></p> 
+                        </div>
+                        <div className="h-fifty flex column z-index-11 stnd-padding">
+                            <ul className="bullets">
+                                <li className="bullets">Legacy, representation</li>
+                                <li className="bullets">Where and how the black dancing body appears and disappears in digital space</li>
+                                <li className="bullets">Who gets to tell what stories?</li>
+                                <li className="bullets">Real and imagined concerns around ownership, intellectual property, copyright, permission, and transmission</li>
+                            </ul>
+                            <a className="small-bttn jc-center sand cobalt-txt top-margin-10px">Presentation</a>
+                        </div>
+                        
+                    </div>
+                    <div className="w-thirty flex column al-center space-btwn stnd-padding">
+                        <div className="w-eighty flex column mobile-top-20px">
+                            <div className="flex sand-gradient-50">
+                                <a className="small-bttn left-margin-10px cobalt sand-txt sand-border">Writing</a>
                             </div>
-                            <div className="w-sixty flex column jc-center mobile-top-20px">
-                                <img src="/Images/Pictures/Reggie.svg" className="side-padding" />
+                            <div className="stnd-padding sand cobalt-txt">
+                                <p className="no-padding txt-al-center"><b> Erin Kilmurray</b></p>
+                                <p className="no-top-padding txt-al-center">My own dance-making has bloomed to me over time, due to the wide availability of artists’ work beyond my own city limits.</p>
                             </div>
                         </div>
-                        <div className="h-forty flex column jc-center stnd-padding">
-                            <div className="flex mobile-column-reverse space-btwn baseline mobile-top-20px">
-                                <p className="no-padding"><b>Ishmael Houston-Jones</b></p>
-                                <a className="small-bttn jc-center sand black-txt">ESSAY</a>
+                        <div className="w-eighty flex column mobile-top-20px">
+                            <div className="flex sand-gradient-50 w-one-hundred">
+                                <a className="small-bttn left-margin-10px cobalt sand-txt sand-border">Writing</a>
                             </div>
-                            <p className="no-padding">The process of my practice seems so fragile and elusive to the idea of archive. My choreography is (almost) never set in the tradition of Modern Dance or Ballet. Therefore, in those infrequent times when I have revived work that was created earlier with a different cast, the new cast is recreating the old work anew. In reality, this is true of the revival of any dance work but this seems much more unambiguous when that work is improvised. Which is “the piece?” What defines a choreography that has no set movement? What are the archival tools available to capture this work? The scattered notes I made in random notebooks hidden away in random drawers and boxes? VHS video tape? Digital transfers? Reviews, previews, and interviews in print or on blogs? Memory?</p>
+                            <div className="stnd-padding sand cobalt-txt">
+                                <p className="no-padding txt-al-center"><b>Alex Springer</b></p>
+                                <p className="no-top-padding txt-al-center">We lose the radical “now” of live performance when we hyper-edit content…</p>
+                            </div>
+                        </div>
+                        <div className="flex column space-btwn stnd-padding">
+                            <div className="flex column mobile-top-20px">
+                                <div className="w-one-hundred sand-txt">
+                                    <p className="no-padding"><b>Columbus, Cori Olinghouse:</b></p>
+                                    <p className="no-top-padding">Different forms promote different modes of reception… Embodied practice gets to speak across body-document divide.</p>
+                                    <a className="small-bttn top-margin-10px cobalt sand-txt sand-border">Presentation</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
